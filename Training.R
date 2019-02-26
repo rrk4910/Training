e64e7203b698c99cc1b4f2d9c2e97e7324cb8026
rrk4910 <45653131+rrk4910@users.noreply.github.com>
2019-02-26 08:45:54 +0530

    Update Training.R

--- Training.R
+++ Training.R
@@ -2,3 +2,17 @@
 read.csv(choose.files())
 read.table(file = "clipboard", sep = /p)
 read.table(choose.files)
+Time series data analysis
+
+# To test stationarity of the original series
+adf.test(rice.yield_ts)
+# To test stationarity of the differenced series
+diff.rice.yield_ts=diff(rice.yield_ts, differences = 12, lag.max=40)
+adf.test(diff.rice.yield_ts)
+
+# To plot acf and pacf of original and differenced series
+acf(rice.yield_ts, lag.max=40)
+pacf(rice.yield_ts, lag.max=40)
+
+acf(diff.rice.yield_ts, lag.max=40)
+pacf(diff.rice.yield_ts, lag.max=40)
